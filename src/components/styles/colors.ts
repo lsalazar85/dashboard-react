d5b7980cf0cb282225fe4d24e7891cbc7dca3519
Luis Salazar <l_salazar85@icloud.com>
2022-11-04 12:57:48 -0300

    wip(main structure): add colors & data object

---
+++ src/components/styles/colors.ts
@@ -0,0 +1,13 @@
+const colors = {
+  White: '#ffffff',
+  Gray: '#E5E6EB',
+  Black: '#000000',
+  Green: '#4DB6AC',
+  Red: '#EF5350',
+  Blue: '#1A237E',
+  LightBlue: '#AFCDFB',
+  SecondaryLightBlue: '#EBF3FA',
+  Orange: '#FFA412',
+};
+
+export default colors;
--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,9 @@ import { Layout } from './components/Layout';
 
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement,
+);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
