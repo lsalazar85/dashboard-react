8dd2eb6d7a7cf16f19a0a3d1613fe5a5fbaed03a
Luis Salazar <l_salazar85@icloud.com>
2022-11-07 17:02:52 -0300

    wip(dashboard data): add dashboard content json

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,3 +1,6 @@
+import { useNavigate } from 'react-router-dom';
+import data from '../../data/data.json';
+
 import { Button } from '../../components/UI/Button';
 import { Card } from '../../components/UI/Card';
 
@@ -16,42 +19,48 @@ import {
 
 import { CardTitleWrapper, CardTitle } from '../../components/UI/Card/styles';
 
-const Dashboard = () => (
-  <DashboardWrapper>
-    <DashboardTitle text="Hello, Luis." fontWeight="600" fontSize="1.75rem" />
-    <DashboardMainSection>
-      <DashboardSide>
-        <Card>
-          <CardTitleWrapper>
-            <CardTitle>Account Balance</CardTitle>
-            <Button text="Cashout" />
-          </CardTitleWrapper>
-          <Amount>$4,250,000</Amount>
-        </Card>
-        <Card border="orange">
-          <MakeNewItemWrapper>
-            <MakeNewItemIcon>
-              <NewPaymentIcon />
-            </MakeNewItemIcon>
-            <MakeNewItem>Make New Payment</MakeNewItem>
-          </MakeNewItemWrapper>
-        </Card>
-        <Card border="green">
-          <MakeNewItemWrapper>
-            <MakeNewItemIcon>
-              <NewInvoiceIcon width="23" height="31" />
-            </MakeNewItemIcon>
-            <MakeNewItem>Make New Invoice</MakeNewItem>
-          </MakeNewItemWrapper>
-        </Card>
-      </DashboardSide>
-      <DashboardSide>
-        <Card>
-          <span>Hello</span>
-        </Card>
-      </DashboardSide>
-    </DashboardMainSection>
-  </DashboardWrapper>
-);
+const Dashboard = () => {
+  const navigate = useNavigate();
+  const dashboardData = data.pages.find((page) => page.name === 'Dashboard')?.content;
+
+  return (
+    <DashboardWrapper>
+      <DashboardTitle text={`${dashboardData?.title}, Luis`} fontWeight="600" fontSize="1.75rem" />
+      <DashboardMainSection>
+        <DashboardSide>
+          <Card>
+            <CardTitleWrapper>
+              <CardTitle>Account Balance</CardTitle>
+              <Button text={`${dashboardData?.accountBalance?.btn}`} />
+            </CardTitleWrapper>
+            <Amount>{`${dashboardData?.accountBalance?.accountTotal}`}</Amount>
+          </Card>
+          {dashboardData?.makePaymentsAndInvoice?.map((item, idx) => (
+            <Card key={idx} border={!idx ? 'orange' : 'green'}>
+              <MakeNewItemWrapper>
+                <MakeNewItemIcon>
+                  {!idx ? <NewPaymentIcon /> : <NewInvoiceIcon width="23" height="31" />}
+                </MakeNewItemIcon>
+                <MakeNewItem>{item.title}</MakeNewItem>
+              </MakeNewItemWrapper>
+            </Card>
+          ))}
+          <Card>
+            <CardTitleWrapper>
+              <CardTitle>{`${dashboardData?.receivables?.title}`}</CardTitle>
+              <Button fn={() => navigate('/receivables')} text={`${dashboardData?.receivables?.btn}`} />
+            </CardTitleWrapper>
+            <Amount>$4,250,000</Amount>
+          </Card>
+        </DashboardSide>
+        <DashboardSide>
+          <Card>
+            <span>Hello</span>
+          </Card>
+        </DashboardSide>
+      </DashboardMainSection>
+    </DashboardWrapper>
+  );
+};
 
 export default Dashboard;
