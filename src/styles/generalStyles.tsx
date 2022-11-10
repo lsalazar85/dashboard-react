9fc2da287f5f7ec8b686cc9d3ca98350c0718540
Luis Salazar <l_salazar85@icloud.com>
2022-11-09 23:28:41 -0300

    wip(select): add select component

---
+++ src/styles/generalStyles.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+import colors from './colors';
+
+export const Select = styled.select`
+  width: 100%;
+  padding: 0.75rem 1.438rem;
+  background: ${colors.White};
+  border: 1px solid ${colors.Gray100};
+  color: ${colors.Gray300};
+  font-size: 1rem;
+  gap: 1.188rem;
+  appearance: none;
+  -webkit-appearance: none;
+`;
